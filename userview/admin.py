0a13aa6f9454ee9f153f09c46b53b13fed475ca1
danish383 <danishshirsangi@gmail.com>
2021-09-11 12:57:22 +0530

    registeres

--- userview/admin.py
+++ userview/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import semFour,semOne,semThree,semTwo
 # Register your models here.
+
+admin.site.register(semOne)
+admin.site.register(semTwo)
+admin.site.register(semThree)
+admin.site.register(semFour)
